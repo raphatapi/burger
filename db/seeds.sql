c4e9dfe8a37ae6c371957c8b1d3cec0de1c2ec36
raphatapi <raphaeltapioca10@gmail.com>
2018-01-05 23:59:52 -0700

    page layout

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Kahuna Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Krusty Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Krabby Patty", false);
