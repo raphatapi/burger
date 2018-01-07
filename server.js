2fdb00c367f739d8afd8275784e66efbd8ec200f
raphatapi <raphaeltapioca10@gmail.com>
2018-01-07 14:34:25 -0700

    update fixed

--- server.js
+++ server.js
@@ -2,24 +2,23 @@ var bodyParser = require("body-parser");
 var express = require("express");
 var methodOverride = require("method-override");
 
-var port = 3000;
+var port = process.env.PORT || 8080;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
+app.listen(port, function(){
+    console.log('The app is listening on port ' + port);
+  });
