048c10cec292de2b495e84bfd33ca5f08e0602d8
raphatapi <raphaeltapioca10@gmail.com>
2018-01-08 13:24:47 -0700

    clearDB

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.env.CLEARDB_DATABASE_URL) {
-  connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+if (process.env.DATABASE_URL) {
+  connection = mysql.createConnection(process.env.DATABASE_URL);
 } else {
   connection = mysql.createConnection({
     port: 3306,
