37e0286511554a17378137b09e6dc0bd16450363
raphatapi <raphaeltapioca10@gmail.com>
2018-01-08 13:28:48 -0700

    get /burger

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
       burgers: data
