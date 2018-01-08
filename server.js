6fd6553ce4c31084ab40e1297986290fa01d4384
raphatapi <raphaeltapioca10@gmail.com>
2018-01-08 13:51:03 -0700

    server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var methodOverride = require("method-override");
 
 var app = express();
 
-app.use(express.static("public"));
+app.use(express.static(process.cwd() + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
