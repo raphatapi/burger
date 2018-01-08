32ed8f887a7f5d68217bfb33933c8c69925cf2b0
raphatapi <raphaeltapioca10@gmail.com>
2018-01-08 13:39:00 -0700

    port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var bodyParser = require("body-parser");
 var express = require("express");
 var methodOverride = require("method-override");
 
+
 var app = express();
 
 app.use(express.static("public"));
@@ -17,8 +18,7 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.set("port", (process.env.PORT || 3000));
-
-app.listen(app.get("port"), function(){
-    console.log('The app is listening on port ' + app.get("port"));
-  });
+var port = process.env.PORT || 3000;
+app.listen(process.env.PORT || 3000, function(){
+  console.log('The app is definitely listening on port ' + port);
+});
