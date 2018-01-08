1c53ba47336a379cc636aafcef8416ecda98d875
raphatapi <raphaeltapioca10@gmail.com>
2018-01-07 21:47:47 -0700

    added db config

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
+  host: "us-cdbr-iron-east-05.cleardb.net",
+  user: "b13d4f5caa667b",
+  password: "4e0c31c2",
+  database: "heroku_2b4c26e35965172"
 });
 
 connection.connect(function(err) {
@@ -17,3 +17,5 @@ connection.connect(function(err) {
 });
 
 module.exports = connection;
+
+// mysql://b13d4f5caa667b:4e0c31c2@us-cdbr-iron-east-05.cleardb.net/heroku_2b4c26e35965172?reconnect=true
